1e5278ee6f7c6d8d9a14e036fdfac51ab6b8e09c
bmalinowsky <bmalinowsky@github.com>
2018-11-23 00:51:44 +0100

    Check option for local device address

--- src/tuwien/auto/calimero/gui/ConnectDialog.java
+++ src/tuwien/auto/calimero/gui/ConnectDialog.java
@@ -88,7 +88,7 @@ class ConnectDialog
 
 		final String local;
 		private final boolean nat;
-		final String localKnxAddress;
+		String localKnxAddress;
 		boolean secure;
 
 		static ConnectArguments newKnxNetIP(final boolean routing, final String localHost,
@@ -170,10 +170,6 @@ class ConnectDialog
 				args.add("--ft12");
 				break;
 			case Tpuart:
-				if (!localKnxAddress.isEmpty()) {
-					args.add("--knx-address");
-					args.add(localKnxAddress);
-				}
 				args.add("--tpuart");
 				break;
 			default:
@@ -183,6 +179,10 @@ class ConnectDialog
 			if (knxMedium != 0) {
 				args.add("--medium");
 				args.add(KNXMediumSettings.getMediumString(knxMedium));
+			}
+			if (!localKnxAddress.isEmpty()) {
+				args.add("--knx-address");
+				args.add(localKnxAddress);
 			}
 			if (!knxAddress.isEmpty()) {
 				if (useRemoteAddressOption)
@@ -373,7 +373,6 @@ class ConnectDialog
 		else if (protocol == Protocol.Tunneling) {
 			usb.setEnabled(false);
 			tpuart.setEnabled(false);
-			localKnxAddress.setEnabled(false);
 		}
 
 		final Composite buttons = new Composite(shell, SWT.NONE);
@@ -412,6 +411,8 @@ class ConnectDialog
 				}
 				else if (!h.isEmpty()) {
 					args = ConnectArguments.newKnxNetIP(useRouting(), local, h, p, natChecked, knxAddr.getText());
+					if (!localKnxAddress.getText().isEmpty())
+						args.localKnxAddress = localKnxAddress.getText();
 					args.secure = secure;
 				}
 				else
