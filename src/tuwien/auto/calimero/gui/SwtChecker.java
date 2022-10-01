af3778dd7bbbadd655bddec9908940862dfc4dd9
bmalinowsky <bmalinowsky@github.com>
2022-10-01 18:06:36 +0200

    Upgrade dependencies

--- src/tuwien/auto/calimero/gui/SwtChecker.java
+++ src/tuwien/auto/calimero/gui/SwtChecker.java
@@ -59,7 +59,7 @@ import org.slf4j.LoggerFactory;
 public class SwtChecker
 {
 	private static final String swtArtifactId = "org.eclipse.swt.";
-	private static final String swtVersion = "3.120.0";
+	private static final String swtVersion = "3.121.0";
 	private static final String baseDownloadUrl = "https://repo.maven.apache.org/maven2/org/eclipse/platform";
 
 	private final Logger logger;
