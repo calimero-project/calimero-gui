b2b83c35a83b6c5e5d500b59f5bb46173ca43dba
bmalinowsky <bmalinowsky@github.com>
2023-02-26 18:19:05 +0100

    Add swt automatic module

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2022, 2022 B. Malinowsky
+    Copyright (c) 2022, 2023 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -34,9 +34,11 @@
     version.
 */
 
+@SuppressWarnings({ "requires-automatic" })
 module io.calimero.gui {
 	requires io.calimero.core;
 	requires io.calimero.tools;
 
+	requires org.eclipse.swt; // automatic module
 	requires org.slf4j;
 }
