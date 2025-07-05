654f683d55ba847274e2d6f4b8127bbcb4f3efb0
bmalinowsky <bmalinowsky@github.com>
2025-07-05 14:56:08 +0200

    Add javadoc for module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,6 @@
 /*
     Calimero 3 - A library for KNX network access
-    Copyright (c) 2022, 2023 B. Malinowsky
+    Copyright (c) 2022, 2025 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -34,6 +34,9 @@
     version.
 */
 
+/**
+ * Calimero SWT-based graphical user interface for KNX communication and management.
+ */
 @SuppressWarnings({ "requires-automatic" })
 module io.calimero.gui {
 	requires io.calimero.core;
