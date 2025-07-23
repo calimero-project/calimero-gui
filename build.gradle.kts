d18e46f2f8898c2eaf1effbb41f44f2ffb6bc86c
bmalinowsky <bmalinowsky@github.com>
2025-07-23 15:59:26 +0200

    Use jdk 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,7 +115,7 @@ dependencies {
 
 java {
 	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(17))
+		languageVersion.set(JavaLanguageVersion.of(21))
 	}
 	withSourcesJar()
 	withJavadocJar()
