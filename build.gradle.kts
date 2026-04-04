6feccf70a472cceb8be8cdd3fba3bbbb83f68461
bmalinowsky <bmalinowsky@github.com>
2026-04-04 16:09:46 +0200

    Prepare for 3.0-M2

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 group = "io.calimero"
-version = "3.0-SNAPSHOT"
+version = "3.0-M2"
 
 fun date(): String = SimpleDateFormat("yyyyMMdd").format(Date())
 val buildClassifier = date()
